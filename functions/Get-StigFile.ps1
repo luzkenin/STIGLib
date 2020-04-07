e3ce0376e1ad271ecdc0f9330f24e5dc37ef614d
luzkenin <luzkenin@gmail.com>
2020-04-07 11:57:34 +0200

    fix basic parsely

--- functions/Get-StigFile.ps1
+++ functions/Get-StigFile.ps1
@@ -5,9 +5,7 @@ function Get-StigFile {
     )
     
     begin {
-        #$try = Invoke-WebRequest -UseBasicParsing -Uri 'https://public.cyber.mil/stigs/downloads/'
-        $StigLibrary = Invoke-WebRequest -Uri 'https://public.cyber.mil/stigs/downloads/'
-        #$Results = @()
+        $StigLibrary = Invoke-WebRequest -Uri 'https://public.cyber.mil/stigs/downloads/' -UseBasicParsing
     }
     
     process {
@@ -15,7 +13,6 @@ function Get-StigFile {
             if($Stig.href -like "*/zip/*"){
         
                 [string]$Name = (($Stig.outerHTML) | % { [regex]::matches( $_ , '(?<=</span>\s+)(.*?)(?=\s+</a>)' ) } | select -ExpandProperty value).trim()
-                #[int]$Version = (($Name) | % { [regex]::matches( $_ , '(?<=Ver\s+)(\d+)(?=,\s+Rel)' )} | select -ExpandProperty value)
                 [int]$Version = (($Name) | % { [regex]::matches( $_ , '(?<=Ver\s+)(\d+)' )} | select -ExpandProperty value)
                 [int]$Release = (($Name) | % { [regex]::matches( $_ , '(?<=,\s+Rel\s+)(\d+)' )} | select -ExpandProperty value) #.trim()
                 [string]$Date = $null#(($Stig.outerHTML) | % { [regex]::matches( $_ , '(?<=<td class="updated_column">\s+)(.*?)(?=\s+</span>)' ) } | select -ExpandProperty value) #-replace "<span style=""display:none;"">",""
@@ -29,7 +26,6 @@ function Get-StigFile {
                 }
             }
         }
-        #$Results
     }
     
     end {
