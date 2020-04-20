56078e4d14b0c39660854c32438f042d80a44b4f
luzkenin <luzkenin@gmail.com>
2020-04-20 10:10:16 +0200

    fix aliases

--- STIGLib/public/Get-StigFile.ps1
+++ STIGLib/public/Get-StigFile.ps1
@@ -25,10 +25,10 @@ function Get-StigFile {
         foreach ($Stig in $StigLibrary.links) {
             if ($Stig.href -like "*/zip/*") {
 
-                [string]$Name = (($Stig.outerHTML) | % { [regex]::matches( $_ , '(?<=</span>\s+)(.*?)(?=\s+</a>)' ) } | select -ExpandProperty value).trim()
-                [int]$Version = (($Name) | % { [regex]::matches( $_ , '(?<=Ver\s+)(\d+)' ) } | select -ExpandProperty value)
-                [int]$Release = (($Name) | % { [regex]::matches( $_ , '(?<=,\s+Rel\s+)(\d+)' ) } | select -ExpandProperty value) #.trim()
-                [string]$Date = $null#(($Stig.outerHTML) | % { [regex]::matches( $_ , '(?<=<td class="updated_column">\s+)(.*?)(?=\s+</span>)' ) } | select -ExpandProperty value) #-replace "<span style=""display:none;"">",""
+                [string]$Name = (($Stig.outerHTML) | ForEach-Object { [regex]::matches( $_ , '(?<=</span>\s+)(.*?)(?=\s+</a>)' ) } | Select-Object -ExpandProperty value).trim()
+                [int]$Version = (($Name) | ForEach-Object { [regex]::matches( $_ , '(?<=Ver\s+)(\d+)' ) } | Select-Object -ExpandProperty value)
+                [int]$Release = (($Name) | ForEach-Object { [regex]::matches( $_ , '(?<=,\s+Rel\s+)(\d+)' ) } | Select-Object -ExpandProperty value) #.trim()
+                [string]$Date = $null#(($Stig.outerHTML) | ForEach-Object { [regex]::matches( $_ , '(?<=<td class="updated_column">\s+)(.*?)(?=\s+</span>)' ) } | Select-Object -ExpandProperty value) #-replace "<span style=""display:none;"">",""
 
                 [PSCustomObject]@{
                     Name    = $Name -replace '[^ -x7e]', ''
