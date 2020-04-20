05de4532ea860c363def011ed822a0fbbfcd2a80
luzkenin <luzkenin@gmail.com>
2020-04-20 09:55:55 +0200

    adding help

--- STIGLib/public/Get-StigFile.ps1
+++ STIGLib/public/Get-StigFile.ps1
@@ -1,34 +1,47 @@
 function Get-StigFile {
+    <#
+    .SYNOPSIS
+    Pulls list of STIG benchmarks
+
+    .DESCRIPTION
+    Get list of STIG benchmark files from https://public.cyber.mil/stigs/downloads/
+
+    .EXAMPLE
+    Get-StigFile
+
+    .NOTES
+    Date doesn't work.
+    #>
     [CmdletBinding()]
     param (
-        
+
     )
-    
+
     begin {
         $StigLibrary = Invoke-WebRequest -Uri 'https://public.cyber.mil/stigs/downloads/' -UseBasicParsing
     }
-    
+
     process {
-        foreach($Stig in $StigLibrary.links){
-            if($Stig.href -like "*/zip/*"){
-        
+        foreach ($Stig in $StigLibrary.links) {
+            if ($Stig.href -like "*/zip/*") {
+
                 [string]$Name = (($Stig.outerHTML) | % { [regex]::matches( $_ , '(?<=</span>\s+)(.*?)(?=\s+</a>)' ) } | select -ExpandProperty value).trim()
-                [int]$Version = (($Name) | % { [regex]::matches( $_ , '(?<=Ver\s+)(\d+)' )} | select -ExpandProperty value)
-                [int]$Release = (($Name) | % { [regex]::matches( $_ , '(?<=,\s+Rel\s+)(\d+)' )} | select -ExpandProperty value) #.trim()
+                [int]$Version = (($Name) | % { [regex]::matches( $_ , '(?<=Ver\s+)(\d+)' ) } | select -ExpandProperty value)
+                [int]$Release = (($Name) | % { [regex]::matches( $_ , '(?<=,\s+Rel\s+)(\d+)' ) } | select -ExpandProperty value) #.trim()
                 [string]$Date = $null#(($Stig.outerHTML) | % { [regex]::matches( $_ , '(?<=<td class="updated_column">\s+)(.*?)(?=\s+</span>)' ) } | select -ExpandProperty value) #-replace "<span style=""display:none;"">",""
-        
+
                 [PSCustomObject]@{
-                    Name = $Name -replace '[^ -x7e]',''
-                    URI = $Stig.href
+                    Name    = $Name -replace '[^ -x7e]', ''
+                    URI     = $Stig.href
                     Version = $Version
                     Release = $Release
-                    Date = $Date
+                    Date    = $Date
                 }
             }
         }
     }
-    
+
     end {
-        
+
     }
 }
--- STIGLib/public/Save-StigFile.ps1
+++ STIGLib/public/Save-StigFile.ps1
@@ -1,8 +1,27 @@
 function Save-StigFile {
+    <#
+    .SYNOPSIS
+    Saves STIG benchmark files
+
+    .DESCRIPTION
+    Saves STIG benchmark files. Accepts pipeline input
+
+    .PARAMETER URI
+    URI to file. Accepts pipeline input
+
+    .PARAMETER Path
+    Path to save file
+
+    .EXAMPLE
+    Get-StigFile | Save-StigFile -Path ".\"
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param (
         # Parameter help description
-        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
         [string[]]
         $URI,
         # Parameter help description
@@ -10,19 +29,19 @@ function Save-StigFile {
         [String]
         $Path
     )
-    
+
     begin {
-        
+
     }
-    
+
     process {
-        foreach($Resource in $URI){
+        foreach ($Resource in $URI) {
             $FileName = ($Resource -split "/")[-1]
             Invoke-WebRequest -Uri $Resource -OutFile $Path\$FileName
         }
     }
-    
+
     end {
-        
+
     }
 }
